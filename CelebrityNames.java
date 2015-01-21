c2b4e1a859a783cbb0f1877d26f3e5647507b5b2
floresz <zabdiel.flores@gmail.com>
2015-01-21 10:43:39 -0800

    changed

--- CelebrityNames.java
+++ CelebrityNames.java
@@ -30,8 +30,10 @@ public class CelebrityNames
         { 
             Scanner sc = new Scanner( text[j] );
             String firstName = sc.next( );
+            String middleName = sc.next();
             String lastName = sc.next( );
-            reversedName[j] = lastName + ", " + firstName;
+            String birthDate = sc.next();
+            reversedName[j] = lastName + ", " + firstName + "" + middleName + "--" + birthDate;
         } 
         
         Arrays.sort(reversedName);
